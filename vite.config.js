434b840015e77b01c9cb7fac54c5773811dfb5b7
Rebin Rafiq <74141327+rebenkurd@users.noreply.github.com>
2025-05-31 23:50:01 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,23 +18,13 @@ export default defineConfig({
   },
 
   // Add proxy for development
-  server: {
+server: {
     proxy: {
       '/api': {
         target: 'https://imsspu.infinityfreeapp.com',
         changeOrigin: true,
         secure: true,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
+        rewrite: (path) => path.replace(/^\/api/, '/api')
       }
     }
   }
